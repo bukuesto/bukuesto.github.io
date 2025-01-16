37cfd2690f2bea3b5f81187cf2c8e457fb307e56
Esto Triramdani N <estolagi@gmail.com>
2025-01-16 21:42:29 +0700

    deploy 16 januari 2025

---
+++ next/static/chunks/app/layout-ff64b1b52c32aaa2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2566:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.bind(n,2110))},9324:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[533,110,441,517,358],()=>s(2566)),_N_E=e.O()}]);
