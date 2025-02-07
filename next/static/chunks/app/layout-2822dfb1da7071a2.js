798321f1e1b681bd3b20dd9a635b64e777bc06cd
Esto Triramdani N <estolagi@gmail.com>
2025-02-07 21:36:59 +0700

    update sitemap and static files; remove old SSG manifests and layouts, add new layout and konfirmasi-pembayaran page

---
+++ next/static/chunks/app/layout-2822dfb1da7071a2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1828:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,9324,23)),Promise.resolve().then(n.bind(n,2110)),Promise.resolve().then(n.t.bind(n,6074,23))},9324:()=>{},6074:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[143,110,441,517,358],()=>s(1828)),_N_E=e.O()}]);
